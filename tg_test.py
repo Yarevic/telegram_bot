c426b6015d02fc803b20bff07fdce4df758062e7
Yarevic <christofdeininger@gmail.com>
2025-08-25 13:41:24 +0200

    Creating local copy of question bank for editing, removing anwered questions from copy, implementation of question counter right&wrong

--- tg_test.py
+++ tg_test.py
@@ -109,14 +109,21 @@ grammar_exercise = [
 
 
 # Hilfsfunktion, die eine zufällige Frage sendet
-async def send_random_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    q = random.choice(grammar_exercise)
+async def send_random_question(update: Update=None, context: ContextTypes.DEFAULT_TYPE=None):
+    if "copy_questions" not in context.user_data:
+        context.user_data["copy_questions"] = grammar_exercise.copy()    
+    copy_questions = context.user_data.get("copy_questions") #Holt die lokale Fragenkopie.
+    if not copy_questions: #Prüft, ob sie leer ist.
+        await update.message.reply_text("🎉 All questions are answered!") #Wenn ja → Nachricht anzeigen und keine neue Frage senden. Wenn nicht → wie gehabt zufällig auswählen.
+        return "end"
+
+    q = random.choice(copy_questions)
     context.user_data["current_q"] = q
-    opts = q["options"][:]   # Kopie der Optionen
+    opts = q["options"][:]   # Kopie der Optionen, [:] alles einschreiben
     random.shuffle(opts)     # mischen für Abwechslung
-    kb = ReplyKeyboardMarkup([opts, ["Back"]], resize_keyboard=True)
+    kb = ReplyKeyboardMarkup([opts, ["Back","Reset"]], resize_keyboard=True)
     await update.message.reply_text(q["sentence"], reply_markup=kb)
-    
+        
     print(q["sentence"])
     print(q["options"][:3])
 
@@ -124,17 +131,27 @@ async def start(update:Update, context:ContextTypes.DEFAULT_TYPE):
     #Asynchrone Startfunktion, wird aufgerufen, wenn der User /start sendet.
     #Мы создали параметр update и вложили туда метод Update из библиотеки Python telegram bot
     # context:ContextTypes.DEFAULT_TYPE мы в переменную передадим контекст полученных сообщений и поддерживаем только дефолтные - текст и комманды
+    context.user_data["copy_questions"] = grammar_exercise.copy()
+    #Damit bekommt jeder Nutzer seine eigene Kopie der für den Nutzer editierbaren Fragenliste, wenn er /start verwendet.
     keyboard = [["Start", "Start training!"],["Contact me"]] #Definiert ein 2-spaltiges Tastenfeld mit zwei Buttons.
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     #Erzeugt eine benutzerdefinierte Tastatur mit den Buttons oben. resize_keyboard=True sorgt dafür, dass die Tastatur auf die Bildschirmgröße angepasst wird.
     await update.message.reply_text("Welcome and choose your action.", reply_markup=reply_markup)
     #Antwortet dem User mit dem Begrüßungstext und zeigt die Tastatur mit den Buttons an.
 
+correct_count = 10
+wrong_count = 0
     
 async def handle_message(update:Update, context:ContextTypes.DEFAULT_TYPE):
     #Funktion zum Verarbeiten beliebiger Textnachrichten, die nicht mit einem / anfangen (also keine Befehle sind).
     text = update.message.text
     #Speichert den Inhalt der empfangenen Nachricht in text.
+    global correct_count
+    context.user_data["correct_count"] = correct_count
+    context.user_data["wrong_count"] = wrong_count
+    correct = context.user_data["correct_count"]
+    wrong = context.user_data["wrong_count"]    
+
     if text == "Start":
         await update.message.reply_text("Thanks for choosing Start.")
     elif text == "Start training!":
@@ -157,15 +174,39 @@ async def handle_message(update:Update, context:ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Choose your actions", reply_markup=reply_markup_exercise)
 
     elif text == "Grammar":
+        if "copy_questions" not in context.user_data:
+            context.user_data["copy_questions"] = grammar_exercise.copy()
+
+            await update.message.reply_text(f"Punktestand: ✅ {correct} | ❌ {wrong}")
+            await send_random_question(update, context)
+##        if not copy_questions:
+##            correct = context.user_data.get("correct_count", 0)
+##            wrong = context.user_data.get("wrong_count", 0)
+##            await update.message.reply_text("🎉 All questions answered!")
+##            await update.message.reply_text(f"🏁 Final state:\n✅ Correct: {correct}\n❌ Wrong: {wrong}")
+##            return
+
+    elif text == "Reset":
+        context.user_data["copy_questions"] = grammar_exercise.copy()
+        context.user_data["correct_count"] = 0
+        context.user_data["wrong_count"] = 0   
+        await update.message.reply_text("✅ Die Fragen wurden zurückgesetzt.")
         await send_random_question(update, context)
         
     elif context.user_data.get("current_q"):
         q = context.user_data["current_q"]
         if text == q["correct"]:
             await update.message.reply_text("✅ Richtig! Weiter so...")
+##            context.user_data["correct_count"] += 1
+            correct_count +=1
+            context.user_data["copy_questions"].remove(q)
+            await update.message.reply_text(f"Punktestand: ✅ {correct} | ❌ {wrong}")
             await send_random_question(update, context)  # nächste Frage
         elif text in q["options"]:
             await update.message.reply_text("❌ Falsch. Versuch’s nochmal.")
+            context.user_data["wrong_count"] += 1
+            await update.message.reply_text(f"Punktestand: ✅ {correct} | ❌ {wrong}")            
+
 
     else:
         await update.message.reply_text("Ich habe dich nicht verstanden. Wähle bitte eine Option.")        
